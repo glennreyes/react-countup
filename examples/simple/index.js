23e151bca4754d0f5cccde269234f8a9defa67d7
Glenn Reyes <glenn@glennreyes.com>
2016-05-26 01:45:34 +0200

    Use render function directly

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import CountUp from '../../lib/CountUp';
 
-ReactDOM.render(
+render(
   <CountUp start={0} end={160526} />,
   document.getElementById('root')
 );
