0b43e6f34c62a3ebe316eb0a8c65f8225b18a758
Glenn Reyes <glenn@glennreyes.com>
2016-05-26 01:03:38 +0200

    Add tests

--- test/CountUp.js
+++ test/CountUp.js
@@ -1,5 +1,46 @@
-describe('CountUp', () => {
-  it('should this and that', () => {
-    // Tests in here
+import { shallow } from 'enzyme';
+import expect from 'expect';
+import React from 'react';
+import CountUp from '../src';
+
+
+describe('<CountUp />', () => {
+  const component = shallow(<CountUp />);
+
+  it('should render an empty span', () => {
+    expect(component.html()).toEqual('<span></span>');
   });
+
+  it('doesn\'t have a className assigned', () => {
+    expect(component.hasClass()).toBe(false);
+  });
+
+});
+
+
+describe('<CountUp className="MyClass" />', () => {
+  const component = shallow(<CountUp className="MyClass" />);
+
+  it('should render an empty span', () => {
+    expect(component.html()).toEqual('<span class="MyClass"></span>');
+  });
+
+  it('has className "MyClass" assigned', () => {
+    expect(component.hasClass()).toBe(false);
+  });
+
+});
+
+
+describe('<CountUp style={{ color: "blue" }} />', () => {
+  const component = shallow(<CountUp style={{ color: "blue" }} />);
+
+  it('should render an empty span', () => {
+    expect(component.html()).toEqual('<span style="color:blue;"></span>');
+  });
+
+  it('has blue color style assigned', () => {
+    expect(component.prop('style').color).toBe('blue');
+  });
+
 });
