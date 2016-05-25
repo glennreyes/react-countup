a1259fa67c5f8d8575ebffcbd65c4c0ac74cdbda
Glenn Reyes <glenn@glennreyes.com>
2016-05-26 01:02:42 +0200

    Change end value in example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import CountUp from '../../lib/CountUp';
 
 ReactDOM.render(
-  <CountUp start={0} end={210987} />,
+  <CountUp start={0} end={160526} />,
   document.getElementById('root')
 );
