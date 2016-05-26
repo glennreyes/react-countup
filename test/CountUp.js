5f89a18797e4d764b2ff90ff2cc1f1b10345ddcc
Glenn Reyes <glenn@glennreyes.com>
2016-05-26 13:13:24 +0200

    Improve coverage

--- test/CountUp.js
+++ test/CountUp.js
@@ -1,15 +1,21 @@
-import 'babel-polyfill';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import expect from 'expect';
 import React from 'react';
 import CountUp from '../src';
 
 
 describe('<CountUp />', () => {
-  const component = shallow(<CountUp />);
+  const component = mount(<CountUp />);
 
-  it('should render an empty span', () => {
-    expect(component.html()).toEqual('<span></span>');
+  it('should render start value 0 as node value', () => {
+    expect(component.text()).toBe('0');
+  });
+
+
+  it('should have another value than 0 as node value', () => {
+    setTimeout(() => {
+      expect(component.text()).toNotBe('0');
+    }, 1000);
   });
 
   it('doesn\'t have a className assigned', () => {
@@ -23,7 +29,7 @@ describe('<CountUp className="MyClass" />', () => {
   const component = shallow(<CountUp className="MyClass" />);
 
   it('should render an empty span', () => {
-    expect(component.html()).toEqual('<span class="MyClass"></span>');
+    expect(component.text()).toEqual('');
   });
 
   it('has className "MyClass" assigned', () => {
@@ -33,15 +39,75 @@ describe('<CountUp className="MyClass" />', () => {
 });
 
 
-describe('<CountUp style={{ color: "blue" }} />', () => {
-  const component = shallow(<CountUp style={{ color: "blue" }} />);
-
-  it('should render an empty span', () => {
-    expect(component.html()).toEqual('<span style="color:blue;"></span>');
-  });
+describe('<CountUp className="MyClass" style={{ color: "blue" }} />', () => {
+  const component = shallow(<CountUp className="MyClass" style={{ color: "blue" }} />);
 
   it('has blue color style assigned', () => {
     expect(component.prop('style').color).toBe('blue');
   });
 
+});
+
+
+describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />', () => {
+  const component = mount(<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />);
+
+  it('should render start value 1000 as node value', () => {
+    expect(component.text()).toBe('1000');
+  });
+
+
+  it('should have another value than 2000 as node value', () => {
+    setTimeout(() => {
+      expect(component.text()).toBe('2000');
+    }, 1000);
+  });
+
+  it('doesn\'t have a className assigned', () => {
+    expect(component.hasClass()).toBe(false);
+  });
+
+});
+
+
+describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} decimals={2} useEasing={false} useGrouping={false} />', () => {
+  const component = mount(<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} decimals={2} useEasing={false} useGrouping={false} />);
+
+  it('should render start value 100.00 as node value', () => {
+    expect(component.text()).toBe('1000.00');
+  });
+
+  it('should have easing disabled', () => {
+    expect(component.prop('useEasing')).toBe(false);
+  });
+
+  it('should have grouping disabled', () => {
+    expect(component.prop('useGrouping')).toBe(false);
+  });
+
+});
+
+
+describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} decimals={2} useEasing={false} useGrouping={true} separator="." decimal="," prefix="EUR " suffix=" left" />', () => {
+  const component = mount(<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} decimals={2} useEasing={false} useGrouping={true} separator="." decimal="," prefix="EUR " suffix=" left" />);
+
+  it('should render start value \'EUR 1.000,00 left\' as node value', () => {
+    expect(component.text()).toBe('EUR 1.000,00 left');
+  });
+
+  it('should have a dot as separator', () => {
+    expect(component.prop('separator')).toBe('.');
+  });
+
+  it('should have a comma as decimal', () => {
+    expect(component.prop('decimal')).toBe(',');
+  });
+
+  it('should render EUR as prefix', () => {
+    expect(component.prop('prefix')).toBe('EUR ');
+  });
+
+  it('should render left as suffix', () => {
+    expect(component.prop('suffix')).toBe(' left');
+  });
 });
---
+++ test/index.js
@@ -0,0 +1,54 @@
+import 'babel-polyfill';
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+// countup.js needs this to work properly in test environment
+global.requestAnimationFrame = window.requestAnimationFrame;
