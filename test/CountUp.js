3722fefc33bad31ffd337edb463a168fe22ceece
Glenn Reyes <glenn@glennreyes.com>
2016-05-26 13:58:40 +0200

    Improve coverage

--- test/CountUp.js
+++ test/CountUp.js
@@ -4,18 +4,15 @@ import React from 'react';
 import CountUp from '../src';
 
 
-describe('<CountUp />', () => {
-  const component = mount(<CountUp />);
+describe('<CountUp start={0} end={100} />', function() {
+  const component = mount(<CountUp start={0} end={100} />);
 
-  it('should render start value 0 as node value', () => {
-    expect(component.text()).toBe('0');
+  it('has 0 as start value assigned', () => {
+    expect(component.prop('start')).toBe(0);
   });
 
-
-  it('should have another value than 0 as node value', () => {
-    setTimeout(() => {
-      expect(component.text()).toNotBe('0');
-    }, 1000);
+  it('has 100 as end value assigned', () => {
+    expect(component.prop('end')).toBe(100);
   });
 
   it('doesn\'t have a className assigned', () => {
@@ -29,7 +26,7 @@ describe('<CountUp className="MyClass" />', () => {
   const component = shallow(<CountUp className="MyClass" />);
 
   it('should render an empty span', () => {
-    expect(component.text()).toEqual('');
+    expect(component.text()).toBe('');
   });
 
   it('has className "MyClass" assigned', () => {
@@ -44,27 +41,6 @@ describe('<CountUp className="MyClass" style={{ color: "blue" }} />', () => {
 
   it('has blue color style assigned', () => {
     expect(component.prop('style').color).toBe('blue');
-  });
-
-});
-
-
-describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />', () => {
-  const component = mount(<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />);
-
-  it('should render start value 1000 as node value', () => {
-    expect(component.text()).toBe('1000');
-  });
-
-
-  it('should have another value than 2000 as node value', () => {
-    setTimeout(() => {
-      expect(component.text()).toBe('2000');
-    }, 1000);
-  });
-
-  it('doesn\'t have a className assigned', () => {
-    expect(component.hasClass()).toBe(false);
   });
 
 });
@@ -110,4 +86,25 @@ describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} en
   it('should render left as suffix', () => {
     expect(component.prop('suffix')).toBe(' left');
   });
+});
+
+
+describe('<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />', () => {
+  const component = mount(<CountUp className="MyClass" style={{ color: "blue" }} start={1000} end={2000} duration={1} />);
+
+  it('should render start value 1000 as node value', () => {
+    expect(component.text()).toBe('1000');
+  });
+
+  it('doesn\'t have a className assigned', () => {
+    expect(component.hasClass()).toBe(false);
+  });
+
+  it('should render bigger value than 1000 after 1 second', (done) => {
+    setTimeout(() => {
+      expect(Math.round(component.text())).toBeGreaterThan(1000);
+    }, 1000);
+    setTimeout(done, 1000);
+  });
+
 });
