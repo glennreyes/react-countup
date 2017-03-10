8eca68bf618ad65a9ec503914731b70ad948b1ff
Glenn Reyes <glenn@glennreyes.com>
2017-03-10 18:15:07 +0100

    v2.0.1

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,72 +1,76 @@
 import React, { Component } from 'react';
 import Highlight from 'react-highlight.js';
-import '!!style!css!highlight.js/styles/solarized-dark.css';
-import 'sanitize.css/sanitize.css';
+import { homepage } from '../package.json';
+import 'highlight.js/styles/solarized-dark.css';
+import 'normalize.css';
 
-import CountUp from 'react-countup';
+import CountUp, { startAnimation } from '../..';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const GitHubCorner = () => (
+  <a href={homepage}>
+    <svg
+      className="Octo"
+      width="80"
+      height="80"
+      viewBox="0 0 250 250"
+    >
+      <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
+      <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" className="Octo__arm" />
+      <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" />
+    </svg>
+  </a>
+);
 
-    this.state = {
-      start: 0,
-      end: (new Date()).getFullYear() || 2016,
-      duration: 3,
-    };
+export default class App extends Component {
+  state = {
+    start: 0,
+    end: (new Date()).getFullYear(), // Just the current year
+    duration: 3,
+  };
 
-    this.startAnimation = this.startAnimation.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
+  handleChange = (event) => {
     const { name, value } = event.target;
 
     this.setState({ [name]: Math.round(value) });
   }
 
-  startAnimation(event) {
-    this.forceUpdate();
+  handleStartAnimation = (event) => {
+    startAnimation(this.myCountUp);
     event.preventDefault();
   }
 
   render() {
     const { start, end, duration } = this.state;
 
-    /* eslint-disable max-len */
-    const githubCorner = (
-      <a href="https://github.com/glennreyes/react-countup">
-        <svg
-          className="Octo"
-          width="80"
-          height="80"
-          viewBox="0 0 250 250"
-        >
-          <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
-          <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" className="Octo__arm" />
-          <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" />
-        </svg>
-      </a>
-    );
-
     return (
       <div className="Wrapper">
         <header>
           <h1>React CountUp</h1>
           <p className="Description">
-            A React component wrapper around <a
-              href="//inorganik.github.io/countUp.js/"
-            >CountUp.js</a>.
+            A React component wrapper around <a href="//inorganik.github.io/countUp.js/">CountUp.js</a>.
             This component counts up a number in an animated way.
           </p>
-          {githubCorner}
+          <GitHubCorner />
         </header>
-
         <section>
           <h2>Demo</h2>
           <div className="Demo">
-            <CountUp className="CountUp" start={start} end={end} duration={duration} />
+            <CountUp
+              className="CountUp"
+              start={start}
+              end={end}
+              duration={duration}
+              ref={(countUp) => {
+                this.myCountUp = countUp;
+              }}
+              onComplete={() => {
+                console.log('Completed! 👏');
+              }}
+              onStart={() => {
+                console.log('Started! 💨');
+              }}
+            />
           </div>
 
           <form className="Form">
@@ -74,10 +78,9 @@ class App extends Component {
               <div className="Form__group">
                 <label className="Label" htmlFor="start">Start: </label>
                 <input
-                  id="start"
                   className="TextField"
-                  type="text"
                   name="start"
+                  type="number"
                   value={start}
                   onChange={this.handleChange}
                 />
@@ -85,10 +88,9 @@ class App extends Component {
               <div className="Form__group">
                 <label className="Label" htmlFor="end">End: </label>
                 <input
-                  id="end"
                   className="TextField"
-                  type="text"
                   name="end"
+                  type="number"
                   value={end}
                   onChange={this.handleChange}
                 />
@@ -96,16 +98,15 @@ class App extends Component {
               <div className="Form__group">
                 <label className="Label" htmlFor="duration">Duration in seconds: </label>
                 <input
-                  id="duration"
                   className="TextField"
-                  type="text"
                   name="duration"
+                  type="number"
                   value={duration}
                   onChange={this.handleChange}
                 />
               </div>
             </div>
-            <button className="Button" onClick={this.startAnimation}>Start animation</button>
+            <button className="Button" onClick={this.handleStartAnimation}>Start animation</button>
           </form>
         </section>
 
@@ -133,13 +134,15 @@ render(
         </section>
 
         <footer className="Footer">
-          <p>Handcrafted with <span
-            className="Emoji"
-          >❤️</span> by <a href="//twitter.com/glnnrys">Glenn Reyes</a></p>
+          <p>
+            With
+            {' '}
+            <span className="Emoji">❤️</span>
+            {' by '}
+            <a href="//twitter.com/glnnrys">Glenn Reyes</a>
+          </p>
         </footer>
       </div>
     );
   }
 }
-
-export default App;
--- demo/src/index.js
+++ demo/src/index.js
@@ -4,5 +4,5 @@ import App from './App';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
