05f41b16634630f7921e3e30988c2910ae44b014
Glenn Reyes <glenn@glennreyes.com>
2017-08-11 01:40:19 +0200

    Remove optional types in props
    As defaultProps will cover undefined props

--- index.js
+++ index.js
@@ -4,52 +4,52 @@ import React, { Component, Element } from 'react';
 import Count from 'countup.js';
 
 type Props = {
-  className?: string,
-  decimal?: string,
-  decimals?: number,
-  duration?: number,
-  easingFn?: () => void,
+  className: string,
+  decimal: string,
+  decimals: number,
+  duration: number,
+  easingFn: () => void,
   end: number,
-  formattingFn?: () => void,
-  onComplete?: () => void,
-  onStart?: () => void,
-  prefix?: string,
-  redraw?: boolean, // eslint-disable-line react/no-unused-prop-types
-  separator?: string,
+  formattingFn: () => void,
+  onComplete: () => void,
+  onStart: () => void,
+  prefix: string,
+  redraw: boolean, // eslint-disable-line react/no-unused-prop-types
+  separator: string,
   start: number,
-  style?: {},
-  suffix?: string,
-  useEasing?: boolean,
-  useGrouping?: boolean,
+  style: {},
+  suffix: string,
+  useEasing: boolean,
+  useGrouping: boolean,
 };
 
 type FormatNumberFn = (
   start: number,
   options: {
-    decimal?: string,
-    decimals?: number,
-    useGrouping?: boolean,
-    separator?: string,
-    prefix?: string,
-    suffix?: string,
+    decimal: string,
+    decimals: number,
+    useGrouping: boolean,
+    separator: string,
+    prefix: string,
+    suffix: string,
   },
 ) => string;
 
 // Adapted from the countup.js format number function
 // https://github.com/inorganik/countUp.js/blob/master/countUp.js#L46-L60
-const formatNumber: FormatNumberFn = (start, options) => {
+export const formatNumber: FormatNumberFn = (start, options) => {
   const num = `${start.toFixed(options.decimals)}`;
   const x = num.split('.');
   let x1 = x[0];
-  const x2 = x.length > 1 ? `${options.decimal || ''}${x[1]}` : '';
+  const x2 = x.length > 1 ? `${options.decimal}${x[1]}` : '';
   const rgx = /(\d+)(\d{3})/;
 
-  if (options.useGrouping) {
+  if (options.useGrouping && options.separator) {
     while (rgx.test(x1)) {
-      x1 = x1.replace(rgx, `$1${options.separator || ''}$2`);
+      x1 = x1.replace(rgx, `$1${options.separator}$2`);
     }
   }
-  return `${options.prefix || ''}${x1}${x2}${options.suffix || ''}`;
+  return `${options.prefix}${x1}${x2}${options.suffix}`;
 };
 
 export const startAnimation = (component: Component<*, *, *>) => {
