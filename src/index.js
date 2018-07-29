1860d6bb625958c0ed73cf03a9198c461e619177
Glenn Reyes <glenn@glennreyes.com>
2018-07-29 12:46:16 +0200

    Initial CountUp component from scratch

--- src/index.js
+++ src/index.js
@@ -1,82 +1,12 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Count from 'countup.js';
 
-// Adapted from the countup.js format number function
-// https://github.com/inorganik/countUp.js/blob/master/countUp.js#L46-L60
-export const formatNumber = (start, options) => {
-  const neg = start < 0;
-  const num = `${Math.abs(start).toFixed(options.decimals)}`;
-  const x = num.split('.');
-  let x1 = x[0];
-  const x2 = x.length > 1 ? `${options.decimal}${x[1]}` : '';
-  const rgx = /(\d+)(\d{3})/;
-
-  if (options.separator) {
-    while (rgx.test(x1)) {
-      x1 = x1.replace(rgx, `$1${options.separator}$2`);
-    }
-  }
-
-  return `${neg ? '-' : ''}${options.prefix || ''}${x1}${x2}${options.suffix ||
-    ''}`;
-};
-
-export const startAnimation = component => {
-  if (!(component && component.spanElement)) {
-    throw new Error(
-      'You need to pass the CountUp component as an argument!\neg. this.myCountUp.startAnimation(this.myCountUp);',
-    );
-  }
-
-  const {
-    decimal,
-    decimals,
-    duration,
-    easingFn,
-    end,
-    formattingFn,
-    onComplete,
-    onStart,
-    prefix,
-    separator,
-    start,
-    suffix,
-    useEasing,
-  } = component.props;
-
-  const countupInstance = new Count(
-    component.spanElement,
-    start,
-    end,
-    decimals,
-    duration,
-    {
-      decimal,
-      easingFn,
-      formattingFn,
-      separator,
-      prefix,
-      suffix,
-      useEasing,
-      useGrouping: !!separator,
-    },
-  );
-
-  if (typeof onStart === 'function') {
-    onStart();
-  }
-
-  countupInstance.start(onComplete);
-};
-
-export default class CountUp extends React.Component {
+class CountUp extends Component {
   static defaultProps = {
-    className: undefined,
     decimal: '.',
     decimals: 0,
-    duration: 3,
     easingFn: null,
-    end: 100,
     formattingFn: null,
     onComplete: undefined,
     onStart: undefined,
@@ -89,56 +19,34 @@ export default class CountUp extends React.Component {
     useEasing: true,
   };
 
-  componentDidMount() {
-    startAnimation(this);
-  }
-
-  shouldComponentUpdate(nextProps) {
-    const hasCertainPropsChanged =
-      this.props.duration !== nextProps.duration ||
-      this.props.end !== nextProps.end ||
-      this.props.start !== nextProps.start;
-
-    return nextProps.redraw || hasCertainPropsChanged;
-  }
-
-  componentDidUpdate() {
-    startAnimation(this);
-  }
-
-  spanElement = null;
-
-  refSpan = span => {
-    this.spanElement = span;
+  static propTypes = {
+    decimal: PropTypes.string,
+    decimals: PropTypes.number,
+    duration: PropTypes.number.isRequired,
+    easingFn: PropTypes.func,
+    end: PropTypes.number.isRequired,
+    formattingFn: PropTypes.func,
+    onComplete: PropTypes.func,
+    onStart: PropTypes.func,
+    prefix: PropTypes.string,
+    separator: PropTypes.string,
+    start: PropTypes.number,
+    suffix: PropTypes.string,
+    style: PropTypes.object,
+    useEasing: PropTypes.bool,
   };
 
-  props;
+  componentDidMount() {
+    this.countUpInstance = new Count(this.spanRef.current, 0, 200);
+
+    this.countUpInstance.start();
+  }
+
+  spanRef = React.createRef();
 
   render() {
-    const {
-      className,
-      start,
-      decimal,
-      decimals,
-      separator,
-      prefix,
-      suffix,
-      style,
-      formattingFn,
-    } = this.props;
-
-    return (
-      <span className={className} ref={this.refSpan} style={style}>
-        {typeof formattingFn === 'function'
-          ? formattingFn(start)
-          : formatNumber(start, {
-              decimal,
-              decimals,
-              separator,
-              prefix,
-              suffix,
-            })}
-      </span>
-    );
+    return <span ref={this.spanRef} />;
   }
 }
+
+export default CountUp;
