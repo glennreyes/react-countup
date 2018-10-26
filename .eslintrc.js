9b8d01e4162227f353013b0d4d025f245667d297
Glenn Reyes <glenn@glennreyes.com>
2018-10-26 10:41:47 +0200

    Bunch of upgrades

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    browser: true,
-    jest: true,
-  },
-  extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
-  parser: 'babel-eslint',
-};
--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.js': ['eslint --fix', 'git add'],
-  '*.{json,md}': ['prettier --write', 'git add'],
-};
