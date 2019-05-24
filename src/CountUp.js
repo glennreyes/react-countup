ef3ffa788ab8adf95cc9af836ad05571eb0c5467
mmarkelov <maks-markel@mail.ru>
2019-05-24 15:53:34 +0300

    Rewrite with hooks

--- src/CountUp.js
+++ src/CountUp.js
@@ -1,140 +1,102 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect, createRef, useRef } from 'react';
 import warning from 'warning';
 import { createCountUpInstance } from './common';
 
-class CountUp extends Component {
-  static propTypes = {
-    decimal: PropTypes.string,
-    decimals: PropTypes.number,
-    delay: PropTypes.number,
-    easingFn: PropTypes.func,
-    end: PropTypes.number.isRequired,
-    formattingFn: PropTypes.func,
-    onEnd: PropTypes.func,
-    onStart: PropTypes.func,
-    prefix: PropTypes.string,
-    redraw: PropTypes.bool,
-    separator: PropTypes.string,
-    start: PropTypes.number,
-    suffix: PropTypes.string,
-    style: PropTypes.object,
-    useEasing: PropTypes.bool,
+const arePropsEqual = (prevProps, nextProps) => {
+  const hasCertainPropsEqual =
+    prevProps.duration === nextProps.duration ||
+    prevProps.end === nextProps.end ||
+    prevProps.start === nextProps.start;
+
+  return hasCertainPropsEqual || !nextProps.redraw;
+};
+
+const CountUp = React.memo(props => {
+  const containerRef = createRef();
+  const timer = useRef();
+  const instance = useRef();
+
+  const createInstance = () => {
+    if (typeof props.children === 'function') {
+      // Warn when user didn't use containerRef at all
+      warning(
+        containerRef.current &&
+          (containerRef.current instanceof HTMLElement ||
+            containerRef.current instanceof SVGTextElement),
+        `Couldn't find attached element to hook the CountUp instance into! Try to attach "containerRef" from the render prop to a an HTMLElement, eg. <span ref={containerRef} />.`,
+      );
+    }
+    return createCountUpInstance(containerRef.current, props);
   };
 
-  static defaultProps = {
-    decimal: '.',
-    decimals: 0,
-    delay: null,
-    duration: null,
-    easingFn: null,
-    formattingFn: null,
-    onEnd: () => {},
-    onPauseResume: () => {},
-    onReset: () => {},
-    onStart: () => {},
-    onUpdate: () => {},
-    prefix: '',
-    redraw: false,
-    separator: '',
-    start: 0,
-    suffix: '',
-    style: undefined,
-    useEasing: true,
-  };
-
-  componentDidMount() {
-    const { children, delay } = this.props;
-    this.instance = this.createInstance();
+  useEffect(() => {
+    const { children, delay } = props;
+    instance.current = createInstance();
 
     // Don't invoke start if component is used as a render prop
     if (typeof children === 'function' && delay !== 0) return;
 
     // Otherwise just start immediately
-    this.start();
-  }
+    start();
 
-  shouldComponentUpdate(nextProps) {
-    const hasCertainPropsChanged =
-      this.props.duration !== nextProps.duration ||
-      this.props.end !== nextProps.end ||
-      this.props.start !== nextProps.start;
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      instance.current.reset();
+    };
+  }, [props.duration, props.start]);
 
-    return hasCertainPropsChanged || this.props.redraw;
-  }
-
-  componentDidUpdate(prevProps) {
-    // If duration or start has changed, there's no way to update the duration
-    // or start value. So we need to re-create the CountUp instance in order to
-    // restart it.
-    if (
-      this.props.duration !== prevProps.duration ||
-      this.props.start !== prevProps.start
-    ) {
-      this.instance = this.createInstance();
-      this.start();
-    }
-
+  useEffect(() => {
     // Only end value has changed, so reset and and re-animate with the updated
     // end value.
-    if (this.props.end !== prevProps.end) {
-      this.instance.reset();
-      this.instance.update(this.props.end);
-    }
-  }
 
-  componentWillUnmount() {
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-    this.instance.reset();
-  }
+    // Don't invoke start if component is used as a render prop
+    if (typeof children === 'function' && props.delay !== 0) return;
 
-  createInstance = () => {
-    if (typeof this.props.children === 'function') {
-      // Warn when user didn't use containerRef at all
-      warning(
-        this.containerRef.current &&
-          (this.containerRef.current instanceof HTMLElement ||
-          this.containerRef.current instanceof SVGTextElement),
-        `Couldn't find attached element to hook the CountUp instance into! Try to attach "containerRef" from the render prop to a an HTMLElement, eg. <span ref={containerRef} />.`,
-      );
-    }
-    return createCountUpInstance(this.containerRef.current, this.props);
-  };
+    instance.current.reset();
+    instance.current.update(props.end);
+  }, [props.end]);
 
-  pauseResume = () => {
-    const { reset, restart: start, update } = this;
-    const { onPauseResume } = this.props;
+  const pauseResume = () => {
+    const { onPauseResume } = props;
 
-    this.instance.pauseResume();
+    instance.current && instance.current.pauseResume();
 
     onPauseResume({ reset, start, update });
   };
 
-  reset = () => {
-    const { pauseResume, restart: start, update } = this;
-    const { onReset } = this.props;
-
-    this.instance.reset();
+  const reset = () => {
+    const { onReset } = props;
 
     onReset({ pauseResume, start, update });
   };
 
-  restart = () => {
-    this.reset();
-    this.start();
+  const restart = () => {
+    reset();
+    start();
   };
 
-  start = () => {
-    const { pauseResume, reset, restart: start, update } = this;
-    const { delay, onEnd, onStart } = this.props;
+  const update = newEnd => {
+    const { onUpdate } = props;
+
+    instance.current && instance.current.update(newEnd);
+
+    onUpdate({ pauseResume, reset, start });
+  };
+
+  const start = () => {
+    const { delay, onEnd, onStart } = props;
     const run = () =>
-      this.instance.start(() => onEnd({ pauseResume, reset, start, update }));
+      instance.current &&
+      instance.current.start(() =>
+        onEnd({ pauseResume, reset, start, update }),
+      );
 
     // Delay start if delay prop is properly set
     if (delay > 0) {
-      this.timeoutId = setTimeout(run, delay * 1000);
+      timer.current = setTimeout(run, delay * 1000);
     } else {
       run();
     }
@@ -142,33 +104,58 @@ class CountUp extends Component {
     onStart({ pauseResume, reset, update });
   };
 
-  update = newEnd => {
-    const { pauseResume, reset, restart: start } = this;
-    const { onUpdate } = this.props;
+  const { children, className, style } = props;
 
-    this.instance.update(newEnd);
-
-    onUpdate({ pauseResume, reset, start });
-  };
-
-  containerRef = React.createRef();
-
-  render() {
-    const { children, className, style } = this.props;
-    const { containerRef, pauseResume, reset, restart, update } = this;
-
-    if (typeof children === 'function') {
-      return children({
-        countUpRef: containerRef,
-        pauseResume,
-        reset,
-        start: restart,
-        update,
-      });
-    }
-
-    return <span className={className} ref={containerRef} style={style} />;
+  if (typeof children === 'function') {
+    return children({
+      countUpRef: containerRef,
+      pauseResume,
+      reset,
+      start: restart,
+      update,
+    });
   }
-}
+
+  return <span className={className} ref={containerRef} style={style} />;
+}, arePropsEqual);
+
+CountUp.propTypes = {
+  decimal: PropTypes.string,
+  decimals: PropTypes.number,
+  delay: PropTypes.number,
+  easingFn: PropTypes.func,
+  end: PropTypes.number.isRequired,
+  formattingFn: PropTypes.func,
+  onEnd: PropTypes.func,
+  onStart: PropTypes.func,
+  prefix: PropTypes.string,
+  redraw: PropTypes.bool,
+  separator: PropTypes.string,
+  start: PropTypes.number,
+  suffix: PropTypes.string,
+  style: PropTypes.object,
+  useEasing: PropTypes.bool,
+};
+
+CountUp.defaultProps = {
+  decimal: '.',
+  decimals: 0,
+  delay: null,
+  duration: null,
+  easingFn: null,
+  formattingFn: null,
+  onEnd: () => {},
+  onPauseResume: () => {},
+  onReset: () => {},
+  onStart: () => {},
+  onUpdate: () => {},
+  prefix: '',
+  redraw: false,
+  separator: '',
+  start: 0,
+  suffix: '',
+  style: undefined,
+  useEasing: true,
+};
 
 export default CountUp;
